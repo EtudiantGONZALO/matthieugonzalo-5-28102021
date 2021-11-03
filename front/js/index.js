f4af4606c701ef4c3cbae2bf3a1141e05450555f
Matthieu Gonzalo <matt04061982@gmail.com>
2021-11-03 22:17:10 +0100

    maj index

--- front/js/index.js
+++ front/js/index.js
@@ -1,30 +1,35 @@
+
+
 // Declaration de la variable et envoi de la réponse de l'api au format JSON
-var kanapfetch = async () => {
-  await fetch("http://localhost:3000/api/products")
-    .then(function(res) {
+kanapfetch = async () => {
+  kanapdata = await fetch("http://localhost:3000/api/products")
+  .then(function(res) {
     if (res.ok) {
       return res.json();
-      }
-    })
-    .then(function(value) {
-      console.log(value);
-    })
-    .catch(function(err) {
-      // Une erreur est survenue
-    })
-}
+    }
+  })
+  .then(function(value) {
+    console.log(value);
+  })
+  .catch(function(err) {
+    // Une erreur est survenue
+  });
+};
 
 
-const kanapdisplay = async () => {
+kanapcards = async () => {
   await kanapfetch();
-  document.getElementById("items").innerHTML = kanapdata.map(kanap)
-    `<a href="${kanap._id}">
-      <article>
-        <img src="${kanap.imageUrl}" alt="${kanap.altTxt}">
-        <h3 class="productName">${kanap.name}</h3>
-        <p class="productDescription">${kanap.description}</p>
-      </article>
-    </a>`;
+  document.getElementById("items").innerHTML = (
+    kanapdata.map(kanapdisplay =>
+      `<a href="./product.html?id=${kanapdisplay._id}">
+        <article>
+          <img src="${kanapdisplay.imageUrl}" alt="${kanapdisplay.altTxt}">
+          <h3 class="productName">${kanapdisplay.name}</h3>
+          <p class="productDescription">${kanapdisplay.description}</p>
+        </article>
+      </a>`
+    )
+  );
 }
 
-kanapdisplay ();
+kanapcards();
