fb044f4598875b63a7df6dffb14ac3fa2f8dee96
Matthieu Gonzalo <matt04061982@gmail.com>
2021-11-03 15:07:41 +0100

    maj index

--- front/js/index.js
+++ front/js/index.js
@@ -1,5 +1,5 @@
-// Envoi de la réponse de l'api au format JSON
-fetch("http://localhost:3000/api/products")
+// Declaration de la variable et envoi de la réponse de l'api au format JSON
+var products = fetch("http://localhost:3000/api/products")
   .then(function(res) {
     if (res.ok) {
       return res.json();
@@ -12,37 +12,13 @@ fetch("http://localhost:3000/api/products")
     // Une erreur est survenue
   });
 
-//Déclaration de la promesse fetch
-var products = fetch("http://localhost:3000/api/products");
-
-// Récupérer ou se situe l'image, le nom et la description
-var productCards = document.querySelector("items");
-var productImageCible = document.querySelector("items" a > article > img); 
-var productNameCible = document.querySelector("items" a > article > h3);
-var productDescriptionCible = document.querySelector("items" a > article > p);
-
-// Récupérer l'image du produit, le nom du produit et la description du produit dans l'api
-var imageProduct = products[0, 1, 2, 3, 4, 5, 6, 7].imageUrl;
-var nameProduct = products[0, 1, 2, 3, 4, 5, 6, 7].name;
-var descriptionProduct = products[0, 1, 2, 3, 4, 5, 6, 7].description;
-
-// Création de chaque cards produit
-class cardsProduct {
-    constructor(productCards, imageProduct, nameProduct, descriptionProduct) {
-        this.productCards = productCards;
-        this.productImageCible = imageProduct;
-        this.productNameCible = nameProduct;
-        this.productDescriptionCible = descriptionProduct;
-    }
-}
-
-// fonction qui affiche l'image, le nom et la description du produit dans la cible de chaque entrée de produit de l'api
-function product() {
-    for (products[0, 1, 2, 3, 4, 5, 6, 7]) {
-        console.log(productCards)
-        console.log(imageProduct)
-        console.log(nameProduct == imageProduct)
-        console.log(descriptionProduct == imageProduct)
-    }
-}
-product();
+  var product = document.getElementById("items");
+  product.innerHTML =
+          <a href="products._id">
+            <article>
+              <img src="products.imageUrl" alt="products.altTxt">
+              <h3 class="products.name">Kanap name1</h3>
+              <p class="products.description">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
+            </article>
+          </a>;
+  
