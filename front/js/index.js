fa1e24d44825f6e0fce575a05ffb3210bb2c5c55
Matthieu Gonzalo <matt04061982@gmail.com>
2021-11-03 17:06:38 +0100

    maj index

--- front/js/index.js
+++ front/js/index.js
@@ -12,15 +12,19 @@ var products = fetch("http://localhost:3000/api/products")
     // Une erreur est survenue
   });
 
-var product = document.getElementById("items");
-product.innerHTML =
-  <a href="products._id">
-    <article>
-      <img src="products.imageUrl" alt="products.altTxt">
-      <h3 class="products.name">Kanap name1</h3>
-      <p class="products.description">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
-    </article>
-  </a>;
+var product = async () => {
+  await products();
+  document.getElementById("items").innerHTML = 
+    `<a href="${products._id}">
+      <article>
+        <img src="${products.imageUrl}" alt="${products.altTxt}">
+        <h3 class="productName">${products.name}</h3>
+        <p class="productDescription">${products.description}</p>
+      </article>
+    </a>;`
+}
+
+product();
 
 for (product of products) {
   console.log(product);
