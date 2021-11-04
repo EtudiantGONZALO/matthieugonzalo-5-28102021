f4ce96cb0e90ba013b04e78664e80f19393c5c29
Matthieu Gonzalo <matt04061982@gmail.com>
2021-11-04 14:54:45 +0100

    maj final index.js

--- front/js/index.js
+++ front/js/index.js
@@ -1,35 +1,37 @@
-
-
 // Declaration de la variable et envoi de la réponse de l'api au format JSON
-kanapfetch = async () => {
-  kanapdata = await fetch("http://localhost:3000/api/products")
+fetch("http://localhost:3000/api/products")
   .then(function(res) {
     if (res.ok) {
       return res.json();
     }
   })
-  .then(function(value) {
-    console.log(value);
+  .then(function(canapes) {
+    //Déclaration de la variable section qui renvoie à l'id "items"
+    var section = document.querySelector(".items");
+    //A partir du data on renvoie une boucle de chaque canapé
+    canapes.forEach(canape => {
+      console.log(canape);
+      //On crée une balise <a> et une balise <article> que l'on apparente
+      var canapeA = document.createElement("a");
+      var article = document.createElement("article");
+      canapeA.appendChild(article);
+      //On affecte au href de la balise <a> un lien
+      canapeA.href = './product.html' + '?id=' + canape._id;
+      //On crée une variable img, h3 et p auquels on attribut des liens
+      var img =  document.createElement("img");
+      img.src = canape.imageUrl;
+      img.alt = canape.altTxt;
+      var h3 = document.createElement("h3");
+      h3.innerText = canape.name;
+      var p = document.createElement("p");
+      p.innerText = canape.description;
+      //On apparente chaque balise a son element parent
+      article.appendChild(img);
+      article.appendChild(h3);
+      article.appendChild(p);
+      section.appendChild(canapeA);     
+    });
   })
   .catch(function(err) {
     // Une erreur est survenue
   });
-};
-
-
-kanapcards = async () => {
-  await kanapfetch();
-  document.getElementById("items").innerHTML = (
-    kanapdata.map(kanapdisplay =>
-      `<a href="./product.html?id=${kanapdisplay._id}">
-        <article>
-          <img src="${kanapdisplay.imageUrl}" alt="${kanapdisplay.altTxt}">
-          <h3 class="productName">${kanapdisplay.name}</h3>
-          <p class="productDescription">${kanapdisplay.description}</p>
-        </article>
-      </a>`
-    )
-  );
-}
-
-kanapcards();
---
+++ front/js/product.js
@@ -0,0 +1,4 @@
+console.log(window.location.href);
+
+//récupérer dans une variable l'id passé en parametre de l'url
+// appeler l'api avec la route /get 1 canape
